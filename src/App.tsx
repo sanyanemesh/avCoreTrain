e9318f75cfbfe8b995f86a88a118ad37ddbfb074
sanyanemesh <sanya.nemesh@gmail.com>
2021-01-04 11:39:24 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { CloudClient } from 'avcore/client';
 import  {Publish}  from './components/publish';
 import {PlayerComponent} from './components/player';
-import { token, client } from './constants'
+import { token, client, tokenEx, clientEx } from './constants'
 
 import './App.css';
 
@@ -12,7 +12,7 @@ export const cloudClient = new CloudClient(client, clientToken);
 function App() {
   const [ready, setReady] = useState<MediaStream | null>(null);
 
-  
+
   console.log(token)
   console.log(client)
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
 export const token = process.env.REACT_APP_DB_TOKEN as string;
 export const client = process.env.REACT_APP_DB_CLIENT as string;
+
+
+export const tokenEx = process.env.REACT_APP_DB_TOKEN_EXAMPLE as string;
+export const clientEx = process.env.REACT_APP_DB_CLIENT_EXAMPLE as string;
