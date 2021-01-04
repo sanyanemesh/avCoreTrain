eb0884d60912d01574868c47382628235156df83
sanyanemesh <sanya.nemesh@gmail.com>
2021-01-04 11:55:24 +0200

    add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CloudClient } from 'avcore/client';
 import  {Publish}  from './components/publish';
 import {PlayerComponent} from './components/player';
@@ -9,23 +9,22 @@ import './App.css';
 export const clientToken = token;
 export const cloudClient = new CloudClient(client, clientToken);
 
+const initmediaDevicesStream = async () => {
+  const mediaDevicesStream = await navigator.mediaDevices.getUserMedia(
+    {video: true, audio: true}
+  )
+  return mediaDevicesStream
+}
+
 function App() {
   const [ready, setReady] = useState<MediaStream | null>(null);
 
-
-  console.log(token)
-  console.log(client)
-
-
-  const initmediaDevicesStream = async () => {
-    const mediaDevicesStream = await navigator.mediaDevices.getUserMedia(
-      {video: true, audio: true}
-    )
-    return mediaDevicesStream
-  }
-  const response =  initmediaDevicesStream().then(function(stream) {
-        return setReady(stream)
-  })
+  useEffect(() => {
+    initmediaDevicesStream().then(function(stream) {
+      return setReady(stream)
+    })
+  },[])
+  
   return (
     <div className="App">
       <header className="App-header">
--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -6,14 +6,14 @@ interface IProps {
   
   export const PlayerComponent = ({ stream }: IProps) => {
     const player = useRef<HTMLVideoElement>(null);
-    const streamReady = (stream !== null)
+    
   
     useEffect(() => {
       if (player.current && stream) {
         player.current.srcObject = stream;
       }
       
-    }, [streamReady]);
+    }, [stream]);
   
     return (
       <video ref={player} muted autoPlay playsInline />
